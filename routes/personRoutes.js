bcc07419765034b3d22f51a632a2f0cf1ad31823
walissons12 <walissons12@hotmail.com>
2021-10-06 14:44:37 -0300

    add verbs put and delete

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
-
 const { application } = require('express')
-const Person = require ('../models/person')
+const Person = require('../models/person')
+
 
 //CREATE
 router.post('/', async (req,res) =>{
@@ -53,7 +53,7 @@ router.get('/:id', async (req,res) => {
     try {
         const person = await Person.findOne({_id: id})
 
-        if(!person){
+        if(!person){ 
             res.status(424).json({message: "O usuario nao foi encontrado!"})
             return
         }
@@ -68,7 +68,53 @@ router.get('/:id', async (req,res) => {
 
 
 //UPDATE (PUT, PATCH)
-router.patch()
+router.patch('/:id', async (req, res) => {
+    const id = req.params.id
+
+    const {name, salary, approved} = req.body
+
+    const person = {
+        name,
+        salary,
+        approved,
+    }
+
+    try {
+        const updatePerson = await Person.updateOne({_id: id}, person)
+
+        if(updatePerson.matchedCount === 0){
+            res.status(424).json({message: "O usuario nao foi encontrado!"})
+            return
+        }
+
+        res.status(200).json(person)
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
+})
+
+//delete
+
+router.delete('/:id', async (req,res) =>{
+    const id = req.params.id
+
+    const person = await Person.findOne({_id: id})
+
+        if(!person){ 
+            res.status(422).json({message: "O usuario nao foi encontrado!"})
+            return
+        }
+
+        try {
+            await Person.deleteOne({_id: id})
+
+            res.status(200).json({message: "Usuario removido com sucesso!!!"})
+
+
+        } catch (error) {
+            res.status(500).json({error: error})
+        }
+}) 
 
 
 module.exports = router
